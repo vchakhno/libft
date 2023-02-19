07c4741223449f1f63390d75160bd2cafd3264fc
Chakhnovski Vélimir <firevelic@gmail.com>
2023-02-19 01:18:36 +0100

    Adjusted the main libft header to point to the correct path of char.h

--- include/libft/libft.h
+++ include/libft/libft.h
@@ -6,33 +6,16 @@
 /*   By: vchakhno <vchakhno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 03:58:20 by vchakhno          #+#    #+#             */
-/*   Updated: 2023/01/30 11:16:46 by vchakhno         ###   ########.fr       */
+/*   Updated: 2023/02/19 01:17:48 by vchakhno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include "libft/text/char.h"
+# include "libft/char.h"
+# include "libft/math.h"
 # include "libft/text/str.h"
 # include "libft/mem/mem.h"
-
-# include <stddef.h>
-
-// ===== Second part =====
-
-// putfd
-void	ft_putchar_fd(char c, int fd);
-void	ft_putendl_fd(char *str, int fd);
-void	ft_putnbr_fd(int n, int fd);
-void	ft_putstr_fd(char *str, int fd);
-
-// stralloc
-char	*ft_itoa(int n);
-char	**ft_split(char const *str, char delim);
-char	*ft_strjoin(char const *str1, char const *str2);
-char	*ft_strmapi(char const *str, char (*f)(unsigned int, char));
-char	*ft_strtrim(char const *str, char const *set);
-char	*ft_substr(char const *str, unsigned int start, size_t len);
 
 #endif
--- include/libft/math.h
+++ include/libft/math.h
@@ -6,14 +6,14 @@
 /*   By: vchakhno <vchakhno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 01:50:01 by vchakhno          #+#    #+#             */
-/*   Updated: 2023/01/30 08:23:25 by vchakhno         ###   ########.fr       */
+/*   Updated: 2023/02/19 01:17:54 by vchakhno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MATH_H
 # define MATH_H
 
-#include <stddef.h>
+# include <stddef.h>
 
 size_t	ft_math_ceil_pow2(size_t n);
 size_t	ft_math_min_size_t(size_t a, size_t b);
