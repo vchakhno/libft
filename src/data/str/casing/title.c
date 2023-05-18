2f7acf7d0eb98d8f338f06c66937ca17a4d8ae16
Velimir Chakhnovski <velimir2707@gmail.com>
2023-05-18 13:05:20 +0200

    Fixed typo in str_title

--- src/data/str/casing/title.c
+++ src/data/str/casing/title.c
@@ -6,7 +6,7 @@
 /*   By: vchakhno <vchakhno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 07:00:48 by vchakhno          #+#    #+#             */
-/*   Updated: 2023/05/18 07:26:45 by vchakhno         ###   ########.fr       */
+/*   Updated: 2023/05/18 13:04:40 by vchakhno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void	ft_str_title(t_any_str *str)
 		if (is_prev_blank)
 			str->c_str[i] = ft_char_to_upper(str->c_str[i]);
 		else
-			str->c_str[i] = ft_char_to_upper(str->c_str[i]);
+			str->c_str[i] = ft_char_to_lower(str->c_str[i]);
 		is_prev_blank = ft_char_is_blank(str->c_str[i]);
 		i++;
 	}
