b355756bb8c04c7103195d4722182f8f55c5a147
Velimir Chakhnovski <velimir2707@gmail.com>
2023-05-31 08:28:15 +0200

    Apparently forgot to implement clamp for signed and unsigned.

--- src/arithmetic/bounds/clamp/clamp_signed.c
+++ src/arithmetic/bounds/clamp/clamp_signed.c
@@ -6,36 +6,29 @@
 /*   By: vchakhno <vchakhno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 10:20:48 by vchakhno          #+#    #+#             */
-/*   Updated: 2023/05/18 07:20:21 by vchakhno         ###   ########.fr       */
+/*   Updated: 2023/05/31 08:26:55 by vchakhno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/arithmetic/bounds.h"
 
-t_i8	ft_i8_max(t_i8 a, t_i8 b)
+t_i8	ft_i8_clamp(t_i8 min, t_i8 value, t_i8 max)
 {
-	if (a > b)
-		return (a);
-	return (b);
+	return (ft_i8_min(ft_i8_max(min, value), max));
 }
 
-t_i16	ft_i16_max(t_i16 a, t_i16 b)
+t_i16	ft_i16_clamp(t_i16 min, t_i16 value, t_i16 max)
 {
-	if (a > b)
-		return (a);
-	return (b);
+	return (ft_i16_min(ft_i16_max(min, value), max));
 }
 
-t_i32	ft_i32_max(t_i32 a, t_i32 b)
+t_i32	ft_i32_clamp(t_i32 min, t_i32 value, t_i32 max)
 {
-	if (a > b)
-		return (a);
-	return (b);
+	return (ft_i32_min(ft_i32_max(min, value), max));
 }
 
-t_i64	ft_i64_max(t_i64 a, t_i64 b)
+t_i64	ft_i64_clamp(t_i64 min, t_i64 value, t_i64 max)
 {
-	if (a > b)
-		return (a);
-	return (b);
+	return (ft_i64_min(ft_i64_max(min, value), max));
 }
+
